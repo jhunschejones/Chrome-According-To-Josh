c4ebc3e22f829e972e1672133649d9dd1d4deb89
Joshua Hunsche Jones <jhunschejones@github.com>
2021-11-02 10:44:29 -0700

    Add additional expected zoom title

--- zoom-cleanup/content-script.js
+++ zoom-cleanup/content-script.js
@@ -1,7 +1,7 @@
-if (
-  document.querySelector("#zoom-ui-frame h1").innerText.replace("\n", ". ") ==
-    "Click Open zoom.us on the dialog shown by your browser. If you don’t see a dialog, click Launch Meeting below"
-  ) {
+const zoomTitle = document.querySelector("#zoom-ui-frame h1").innerText.replace("\n", ". ");
+const expectedZoomTitles = ["Your meeting has been launched. Don’t see your Zoom meeting?", "Click Open zoom.us on the dialog shown by your browser. If you don’t see a dialog, click Launch Meeting below"]
+
+if (expectedZoomTitles.includes(zoomTitle)) {
   // We're on the tab we want to cleanup
   setTimeout(() => {
     window.close();
