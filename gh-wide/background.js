60fb04431ab5ef6e7742c1f34ff3178e37df8a85
Joshua Hunsche Jones <jhunschejones@github.com>
2022-01-06 11:33:59 -0600

    Expand gh-wide support to README's as well

--- gh-wide/background.js
+++ gh-wide/background.js
@@ -1,5 +1,6 @@
 chrome.action.onClicked.addListener((tab) => {
-  if (tab.url.match(/https:\/\/.*github\.com\/.*\/(pull|issues)\/*/)) {
+  // Restrict to just pulls or issues with: /https:\/\/.*github\.com\/.*\/(pull|issues)\/*/
+  if (tab.url.match(/https:\/\/.*github\.com\/*/)) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: ["content-script.js"]
--- gh-wide/content-script.js
+++ gh-wide/content-script.js
@@ -1,4 +1,4 @@
-var rightBar = document.querySelector("#partial-discussion-sidebar");
+var rightBar = document.querySelector("#partial-discussion-sidebar") || document.querySelector(".Layout-sidebar");
 var mainLayout = document.querySelector(".Layout.Layout--flowRow-until-md.Layout--sidebarPosition-end.Layout--sidebarPosition-flowRow-end");
 
 function wideView() {
@@ -11,9 +11,12 @@ function defaultView() {
   mainLayout.style.display = "grid";
 }
 
-// Toggle between wide view and default view when the script runs
-if (rightBar.style.display == "none") {
-  defaultView();
-} else {
-  wideView();
+// Don't do anything if the required elements are not present
+if (rightBar && mainLayout) {
+  // Toggle between wide view and default view when the script runs
+  if (rightBar.style.display == "none") {
+    defaultView();
+  } else {
+    wideView();
+  }
 }
