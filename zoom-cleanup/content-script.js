ec661a4598fabf2254c84fd36005ad9b021d125a
Joshua Hunsche Jones <jhunschejones@github.com>
2022-01-10 16:54:23 -0600

    Move page load setTimeout to the right spot

--- zoom-cleanup/content-script.js
+++ zoom-cleanup/content-script.js
@@ -1,9 +1,12 @@
-const zoomTitle = document.querySelector("#zoom-ui-frame h1").innerText.replace("\n", ". ");
-const expectedZoomTitles = ["Your meeting has been launched. Don’t see your Zoom meeting?", "Click Open zoom.us on the dialog shown by your browser. If you don’t see a dialog, click Launch Meeting below"]
+const expectedZoomTitles = [
+  "Your meeting has been launched. Don’t see your Zoom meeting?",
+  "Click Open zoom.us on the dialog shown by your browser. If you don’t see a dialog, click Launch Meeting below"
+];
 
-if (expectedZoomTitles.includes(zoomTitle)) {
-  // We're on the tab we want to cleanup
-  setTimeout(() => {
+setTimeout(() => {
+  const zoomTitle = document.querySelector("#zoom-ui-frame h1").innerText.replace("\n", ". ");
+  if (expectedZoomTitles.includes(zoomTitle)) {
+    // We're on the tab we want to cleanup
     window.close();
-  }, 1000);
-}
+  }
+}, 600);
