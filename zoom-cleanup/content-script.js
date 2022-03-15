6d8f8b20e77fd03e34ca3e0f71c00b5173e7c4f1
Joshua Hunsche Jones <jhunschejones@github.com>
2022-03-15 12:07:25 -0500

    Attempting to improve resiliency with retries

--- zoom-cleanup/content-script.js
+++ zoom-cleanup/content-script.js
@@ -1,12 +1,23 @@
+console.log("Running zoom-cleanup...")
+
 const expectedZoomTitles = [
   "Your meeting has been launched. Don’t see your Zoom meeting?",
   "Click Open zoom.us on the dialog shown by your browser. If you don’t see a dialog, click Launch Meeting below"
 ];
+const maxRetries = 15;
+let retries = 0;
 
-setTimeout(() => {
+const interval = setInterval(() => {
   const zoomTitle = document.querySelector("#zoom-ui-frame h1").innerText.replace("\n", ". ");
+  // We're on the tab we want to cleanup
   if (expectedZoomTitles.includes(zoomTitle)) {
-    // We're on the tab we want to cleanup
     window.close();
   }
-}, 600);
+
+  if (retries == maxRetries) {
+    clearInterval(interval);
+  } else {
+    console.log("Retrying zoom-cleanup...");
+    retries += 1;
+  }
+}, 1000);
