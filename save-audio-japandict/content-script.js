4caa396aae16183b4b31c3305e78b7f1ac37dd6b
Joshua Hunsche Jones <jhunschejones@github.com>
2022-03-24 16:35:03 -0500

    Update download URLs to work with Anki's auto-download feature

--- save-audio-japandict/content-script.js
+++ save-audio-japandict/content-script.js
@@ -2,11 +2,17 @@ function makeDownloadLinkFromAudioSource(audioSourceElement) {
   let url, text, token;
   [url, text, token] = JSON.parse(audioSourceElement.dataset.reading);
 
+  // const possibleFileName = Array.from(
+  //   document.querySelectorAll("div[lang='en']")
+  // ).map((e) => e.innerText).join(", ");
+
   const a = document.createElement("a");
   a.appendChild(document.createTextNode("💾"));
   a.title = "Download or copy";
   a.ariaLabel = "Download or copy";
-  a.href = `https:${url}/read?text=${text}&outputFormat=mp3&jwt=${token}`;
+  // 💁🏼 The `fileFormatForAnkiDownload` query param is only in this URL to activate Anki's
+  // auto-download feature, it is not required to build a valid audio link.
+  a.href = `https:${url}/read?text=${text}&outputFormat=mp3&jwt=${token}&fileFormatForAnkiDownload=.mp3`;
   a.download = `${text}.mp3`;
   a.target = "_blank"; // point the link at a new window in case it's clicked
   a.style.userSelect = "none";
