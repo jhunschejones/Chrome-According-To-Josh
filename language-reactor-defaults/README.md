f9e34bfca6ccab748797f933145704899898882b
Joshua Hunsche Jones <jhunschejones@github.com>
2022-06-28 10:50:50 -0500

    Code cleanup in language-reactor-defaults

--- language-reactor-defaults/README.md
+++ language-reactor-defaults/README.md
@@ -1,27 +1,8 @@
 # Language Reactor Defaults
 
 ### Description
-Language Reactor is a great tool for language learning on video sites like Netflix and YouTube. This extension sets some sensible defaults on page load so you don't have to play with the DOM or bookmarklets. These include:
+Language Reactor is a powerful tool for language learning on video sites like Netflix and YouTube. This extension sets some sensible defaults on page load for learners interested in using the tool for Refold sentence mining. Defaults include:
 - Clean copy subtitles without furigana
+- Hide hover translations on the bottom of the page
 - Hide hover translations in side panel by default
-- Hide hover translations on the bottom of the page (via `ldr-styles.css`)
-
-### Bonus
-If you wish to toggle translations in the side, subtitles panel, you can create a bookmarklet with the following JavaScript:
-
-```javascript
-// The code
-(() => {
- var sideTranslation = document.querySelector("#lln-v-sub-hover-translation");
- if (sideTranslation.style.display == "none" || sideTranslation.style.display == "") {
-   sideTranslation.style.display = "block";
- } else {
-   sideTranslation.style.display = "none";
- }
-})();
-
-// As a bookmarklet
-javascript:((() => { var sideTranslation = document.querySelector("#lln-v-sub-hover-translation"); if (sideTranslation.style.display == "none" || sideTranslation.style.display == "") { sideTranslation.style.display = "block"; } else { sideTranslation.style.display = "none"; } })())
-```
-
-Or simply use the `command + e` keyboard shortcut!
+- Enable toggling side panel translations on and off with `command + e`
--- language-reactor-defaults/content-script.js
+++ language-reactor-defaults/content-script.js
@@ -1,8 +1,55 @@
-// Wait for language reactor extension to load after page load
-setTimeout(() => {
-  // Clean copy subtitles without furigana
-  (() => {
-    document.addEventListener("copy", (event) => {
+(() => {
+  const keyboardShortcuts = {
+    keysPressed: {},
+    shortcutTimeout: null,
+    parseKeyboardShortcuts: (e) => {
+      switch (e.key) {
+        case "Alt":
+          keyboardShortcuts.keysPressed["option"] = true;
+          break;
+        case "Meta":
+          keyboardShortcuts.keysPressed["command"] = true;
+          break;
+        default:
+          if (!e.key) {
+            return;
+          }
+          keyboardShortcuts.keysPressed[e.key.toLowerCase()] = true;
+      }
+
+      if (keyboardShortcuts.keysPressed["command"] && keyboardShortcuts.keysPressed["e"]) {
+        e.preventDefault();
+
+        const sideTranslation = document.querySelector("#lln-v-sub-hover-translation");
+        if (!sideTranslation) {
+          return console.log("#lln-v-sub-hover-translation not found");
+        }
+
+        if (sideTranslation.style.display == "none" || sideTranslation.style.display == "") {
+          return sideTranslation.style.display = "block";
+        } else {
+          return sideTranslation.style.display = "none";
+        }
+      }
+    },
+    reset: () => {
+      keyboardShortcuts.keysPressed = {};
+    },
+    execute: (e) => {
+      // short manual de-bounce
+      clearTimeout(keyboardShortcuts.shortcutTimeout);
+      keyboardShortcuts.shortcutTimeout = setTimeout(() => {
+        keyboardShortcuts.parseKeyboardShortcuts(e);
+      }, 100);
+    },
+  }
+
+  document.addEventListener("keydown", (e) => keyboardShortcuts.execute(e));
+  document.addEventListener("keyup", () => keyboardShortcuts.reset());
+
+
+  const cleanCopy = {
+    copyWithoutFurigana: (e) => {
       const furigana = Array.from(document.querySelectorAll(".lln-vertical-view .lln-word .translit"));
       let furiganaVisibility;
       if (furigana[0]) {
@@ -10,7 +57,7 @@ setTimeout(() => {
         furiganaVisibility = furigana[0].style.visibility;
       }
       // Hide furigana before copying
-      furigana.map((e) => e.style.visibility = "hidden");
+      furigana.map((f) => f.style.visibility = "hidden");
 
       // Clean copy
       const selection = document.getSelection();
@@ -20,61 +67,13 @@ setTimeout(() => {
         .split(lineBreaks)
         .filter((item) => !item.match(lineBreaks)) // remove line breaks from array
         .join("");
-      event.clipboardData.setData("text/plain", textToCopy);
-      event.preventDefault();
+      e.clipboardData.setData("text/plain", textToCopy);
+      e.preventDefault();
 
       // Reset furigana visibility
-      furigana.map((e) => e.style.visibility = furiganaVisibility);
-    });
-  })();
-}, 5000);
-
-
-// Add bonus keyboard shortcuts
-(() => {
-  const bonusShortcuts = {
-    keysPressed: {},
-    shortcutTimeout: null,
-    parseKeyboardShortcuts: (e) => {
-      switch (e.key) {
-        case "Alt":
-          bonusShortcuts.keysPressed["option"] = true;
-          break;
-        case "Meta":
-          bonusShortcuts.keysPressed["command"] = true;
-          break;
-        default:
-          if (!e.key) {
-            return;
-          }
-          bonusShortcuts.keysPressed[e.key.toLowerCase()] = true;
-      }
-
-      if (bonusShortcuts.keysPressed["command"] && bonusShortcuts.keysPressed["e"]) {
-        e.preventDefault();
-
-        const sideTranslation = document.querySelector("#lln-v-sub-hover-translation");
-        if (!sideTranslation) {
-          return console.log("#lln-v-sub-hover-translation not found");
-        } else if (sideTranslation.style.display == "none" || sideTranslation.style.display == "") {
-          return sideTranslation.style.display = "block";
-        } else {
-          return sideTranslation.style.display = "none";
-        }
-      }
-    },
-    reset: () => {
-      bonusShortcuts.keysPressed = {};
-    },
-    execute: (e) => {
-      // short manual de-bounce
-      clearTimeout(bonusShortcuts.shortcutTimeout);
-      bonusShortcuts.shortcutTimeout = setTimeout(() => {
-        bonusShortcuts.parseKeyboardShortcuts(e);
-      }, 100);
+      furigana.map((f) => f.style.visibility = furiganaVisibility);
     },
   }
 
-  document.addEventListener("keydown", (e) => bonusShortcuts.execute(e));
-  document.addEventListener("keyup", () => bonusShortcuts.reset());
+  document.addEventListener("copy", (e) => cleanCopy.copyWithoutFurigana(e));
 })();
