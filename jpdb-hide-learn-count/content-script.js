1705bfebcbbe5fe18e9acbe8bcb76d8d6feb9c20
Joshua Hunsche Jones <jhunschejones@github.com>
2023-11-03 16:24:44 -0500

    Add saftey check

--- jpdb-hide-learn-count/content-script.js
+++ jpdb-hide-learn-count/content-script.js
@@ -1,1 +1,1 @@
-[...document.querySelectorAll("[style*='color: red;']")][0].parentElement.textContent = "Learn";
+if ([...document.querySelectorAll("[style*='color: red;']")][0]) [...document.querySelectorAll("[style*='color: red;']")][0].parentElement.textContent = "Learn";
